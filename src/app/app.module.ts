27bd064fc94eba95a3ba6541a44f38f566f97d5b
Samuel Simão <samuelsimao.dev@gmail.com>
2022-01-16 19:55:11 -0300

    setup initial components and README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShelfComponent } from './components/shelf/shelf.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShelfComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
